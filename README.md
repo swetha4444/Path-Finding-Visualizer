bbef7936a3fa9b9051bcd0ac7654691fa6de2ac9
SwethaS <swethasaseendran4@gmail.com>
2021-06-08 03:33:27 +0530

    updates

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# Path-Finding-Visualizer
+# Path Finding Visualizer
+
+## Algorithms
+<ul>
+    <li>A* Search<li>
+    <li> Breadth First Search<li>
+    <li> Depth First Search<li>
+</ul>
+
+## Requirements
+<ol>
+    <li>Python 3<li>
+    <li>TKinter<li>
+    <li>Pygame<li>
+</ol>
+
+## Installation
+To run the app in a  windows environment
+ 1. Install python 3.8
+ 2. Run ```pip install pygame```,```pip install tkinter```
+ 3. Clone/Download the files
+ 4. Run ```python path.py``` to start the application
+
--- path.py
+++ path.py
@@ -12,6 +12,7 @@ from cell import Cell
 screen = pygame.display.set_mode((600, 600))
 screen.fill((255, 255, 255))
 
+
 #* -----------------------  Initialize Variables    -----------------------
 cols = 50
 row = 50
@@ -21,29 +22,27 @@ purple = (128,0,128)
 teal = (173,216,230)
 blue = (0, 0, 255)
 black = (0, 0, 0)
-end.show((255, 50, 50), 0)
-start.show((50, 255, 50), 0)
 w = 600 / cols
 h = 600 / row
 cameFrom = []
 grid = [0 for i in range(cols)]
 
-
 #* -----------------------  Creating Grid   -----------------------
-# create 2d array
+# Create 2D array
 for i in range(cols):
     grid[i] = [0 for i in range(row)]
+
 # Create Spots
 for i in range(cols):
     for j in range(row):
         grid[i][j] = Cell(pygame, screen, w, h, row, cols, i, j)
+
 # Display Grid
 for i in range(cols):
     for j in range(row):
         grid[i][j].show((0, 0, 0), 1)
 
-
-#* -----------------------  Marking boundaries  -----------------------
+#Marking boundaries
 for i in range(0,row):
     grid[0][i].show(black, 0)
     grid[0][i].obs = True
@@ -65,17 +64,28 @@ h_option = 'Euclidean'
 #initial layout choice
 m_option = 'Blank'
 
+
 #* -----------------------  Create Menu -----------------------
+#Handle Submit
+def onsubmit():
+    global start
+    global end
+    st = startBox.get().split(',')
+    ed = endBox.get().split(',')
+    start = grid[int(st[0])][int(st[1])]
+    end = grid[int(ed[0])][int(ed[1])]
+    window.quit()
+    window.destroy()
+
 window = Tk()
-label = Label(window, text='Start (x,y) : ')
 
 # Start Cell
+label = Label(window, text='Start (x,y) : ')
 startBox = Entry(window)
-startBox.grid(row=0, column=1, pady=3)
 
 #End Cell
-endBox = Entry(window)
 label1 = Label(window, text='End (x,y) : ')
+endBox = Entry(window)
 
 #Menu Options
 var = IntVar()
@@ -85,7 +95,7 @@ heur = StringVar(window)
 mazeType = StringVar(window)
 
 #List with options
-choices = [ 'A*','Dijkstra','DFS','BFS' ]
+choices = [ 'A*','DFS','BFS' ]
 h_choices = [ 'Euclidean', 'Manhattan']
 m_choices = [ 'Blank','Random' ]
 
@@ -104,8 +114,8 @@ hMenu = OptionMenu(window, heur, *h_choices, command=change_heuristic)
 #Algo Dropdown Menu
 def change_dropdown(*args):
     global choices
-    c_option = heur.get()
-    print( c_option )
+    option = algoType.get()
+    print( option )
 popupMenu = OptionMenu(window, algoType, *choices, command=change_dropdown)
 
 #Maze Dropdown Menu
@@ -115,16 +125,7 @@ def change_layout(*args):
     print( m_option )
 mMenu = OptionMenu(window, mazeType, *m_choices, command=change_layout)
 
-#Handle Submit
-def onsubmit():
-    global start
-    global end
-    st = startBox.get().split(',')
-    ed = endBox.get().split(',')
-    start = grid[int(st[0])][int(st[1])]
-    end = grid[int(ed[0])][int(ed[1])]
-    window.quit()
-    window.destroy()
+
 submit = Button(window, text='Submit', command=onsubmit)
 
 #Adding Labels
@@ -159,13 +160,227 @@ if m_option == 'Random':
                 grid[i][j].obs = True
                 grid[i][j].show(black, 0)
 
+#Block Nodes
 def mousePress(x):
     t = x[0]
     w = x[1]
     g1 = t // (600 // cols)
     g2 = w // (600 // row)
-    acess = grid[g1][g2]
-    if acess != start and acess != end:
-        if acess.obs == False:
-            acess.obs = True
-            acess.show(black, 0)
+    block = grid[g1][g2]
+    if block != start and block != end:
+        if block.obs == False:
+            block.obs = True
+            block.show(black, 0)
+
+#Display Start and End Nodes
+end.show((255, 50, 50), 0)
+start.show((50, 255, 50), 0)
+
+#Display Block Nodes
+loop = True
+while loop:
+    ev = pygame.event.get()
+
+    for event in ev:
+        if event.type == pygame.QUIT:
+            pygame.quit()
+        if pygame.mouse.get_pressed()[0]:
+            try:
+                pos = pygame.mouse.get_pos()
+                mousePress(pos)
+            except AttributeError:
+                pass
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                loop = False
+                break
+
+
+#* -----------------------  Execute Algorithm -----------------------
+# Find Neighbours for all Cells
+for i in range(cols):
+    for j in range(row):
+        grid[i][j].addNeighbors(grid)
+
+#Decide type of heurisitic
+def heurisitic(n, e):
+    if h_option == 'Euclidean':
+        d = math.sqrt((n.i - e.i)**2 + (n.j - e.j)**2)
+    elif h_option == 'Manhattan':
+        d = abs(n.i - e.i) + abs(n.j - e.j)
+    return d
+
+def a_star():
+    end.show((255, 50, 50), 0)
+    start.show((50, 255, 50), 0)
+    while len(openSet) > 0:
+        lowestIndex = 0
+        for i in range(len(openSet)):
+            if openSet[i].f < openSet[lowestIndex].f:
+                lowestIndex = i
+
+        current = openSet[lowestIndex]
+        if current == end:
+            print('done', current.f)
+            start.show((50, 255, 50),0)
+            temp = current.f
+            for i in range(round(current.f)):
+                current.closed = False
+                current.show((0,0,255), 0)
+                current = current.previous
+            end.show((255, 50, 50), 0)
+
+            Tk().wm_withdraw()
+            result = messagebox.askokcancel('Program Finished', ('The program finished, the shortest distance/least weighted path \n is ' + str(temp) + '\n would you like to re run the program?'))
+            if result == True:
+                os.execl(sys.executable,sys.executable, *sys.argv)
+            else:
+                ag = True
+                while ag:
+                    ev = pygame.event.get()
+                    for event in ev:
+                        if event.type == pygame.KEYDOWN:
+                            ag = False
+
+                            break
+            pygame.quit()
+
+        openSet.pop(lowestIndex)
+        closedSet.append(current)
+
+        neighbors = current.neighbors
+        for i in range(len(neighbors)):
+            neighbor = neighbors[i]
+            if neighbor not in closedSet:
+                tempG = current.g + current.value
+                if neighbor in openSet:
+                    if neighbor.g > tempG:
+                        neighbor.g = tempG
+                else:
+                    neighbor.g = tempG
+                    openSet.append(neighbor)
+                    if var.get():
+                        neighbor.show(teal,0)
+
+            neighbor.h = heurisitic(neighbor, end)
+            neighbor.f = neighbor.g + neighbor.h
+
+            if neighbor.previous == None:
+                neighbor.previous = current
+        
+        if var.get() and current != start:
+            current.show(purple,0)
+        current.closed = True
+
+def dfs():
+    end.show((255, 50, 50), 0)
+    start.show((50, 255, 50), 0)
+    while len(openSet) > 0:
+        current = openSet.pop(-1)
+        if current == end:
+            print('done', current.f)
+            start.show((50, 255, 50),0)
+            temp = current.f
+            for i in range(round(current.f)):
+                current.closed = False
+                current.show((0,0,255), 0)
+                current = current.previous
+            end.show((255, 50, 50), 0)
+
+            Tk().wm_withdraw()
+            result = messagebox.askokcancel('Program Finished', ('The program finished, the shortest distance \n to the path is ' + str(temp) + ' blocks away, \n would you like to re run the program?'))
+            if result == True:
+                os.execl(sys.executable,sys.executable, *sys.argv)
+            else:
+                ag = True
+                while ag:
+                    ev = pygame.event.get()
+                    for event in ev:
+                        if event.type == pygame.KEYDOWN:
+                            ag = False
+                            break
+            pygame.quit()
+            break
+
+        neighbors = current.neighbors
+        for i in range(len(neighbors)):
+            if not neighbors[i] in closedSet:
+                openSet.append(neighbors[i])
+                neighbors[i].previous = current
+                neighbors[i].f = current.f + 1
+                if var.get():
+                    neighbors[i].show(teal,0)
+
+        closedSet.append(current)
+        if var.get() and current != start:
+            current.show(purple,0)        
+        
+        current.closed = True
+
+def bfs():
+    end.show((255, 50, 50), 0)
+    start.show((50, 255, 50), 0)
+    current = start
+    current.closed = True
+    closedSet.append(current)
+    while len(openSet) > 0:
+        current = openSet.pop(0)
+        if var.get() and current != start:
+            current.closed = False
+            current.show(purple,0) 
+            current.closed = True
+
+        if current == end:
+            print('done', current.f)
+            start.show((50, 255, 50),0)
+            temp = current.f
+            for i in range(round(current.f)):
+                current.closed = False
+                current.show((0,0,255), 0)
+                current = current.previous
+            end.show((255, 50, 50), 0)
+
+            Tk().wm_withdraw()
+            result = messagebox.askokcancel('Program Finished', ('The program finished, the shortest distance \n to the path is ' + str(temp) + ' blocks away, \n would you like to re run the program?'))
+            if result == True:
+                os.execl(sys.executable,sys.executable, *sys.argv)
+            else:
+                ag = True
+                while ag:
+                    ev = pygame.event.get()
+                    for event in ev:
+                        if event.type == pygame.KEYDOWN:
+                            ag = False
+                            break
+            pygame.quit()
+            break
+
+        neighbors = current.neighbors
+        for i in range(len(neighbors)):
+            if not neighbors[i] in closedSet:
+                openSet.append(neighbors[i])
+                neighbors[i].previous = current
+                neighbors[i].f = current.f + 1
+                if var.get():
+                    neighbors[i].show(teal,0)
+                neighbors[i].closed = True
+                closedSet.append(neighbors[i])  
+
+def main():
+    print( option )
+    print( m_option )
+    if option == 'A*':
+        print( h_option )
+        a_star()
+    elif option == 'DFS':
+        dfs()
+    elif option == 'BFS':
+        bfs()
+
+while True:
+    ev = pygame.event.poll()
+    if ev.type == pygame.QUIT:
+        pygame.quit()
+    pygame.display.update()
+    main()
+
